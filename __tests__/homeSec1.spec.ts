2aa36e786bfcd423dbd2e65cc9a4b4be624c75c6
castelo <castlogomaa459@gmail.com>
2025-02-25 00:06:19 +0200

    homeSec 1 tested out

---
+++ __tests__/homeSec1.spec.ts
@@ -0,0 +1,16 @@
+import {mountSuspended} from "@nuxt/test-utils/runtime";
+import {test, describe, expect} from "vitest";
+import HomeSec1 from "~/components/HomeSec1.vue";
+
+describe("testing section 1 of home page", () => {
+    test("testing content", async () => {
+        const homeSec1 = await mountSuspended(HomeSec1);
+        expect(homeSec1.get("h1").text()).toBe("Find the best  talent");
+        const searchKeywords = ["global", "abundance", "performing", "that."];
+        for (let i = 0; i < searchKeywords.length; i++) {
+            expect(
+                homeSec1.get("article").get("p").text().split(" ")
+            ).toContain(searchKeywords[i] as string);
+        }
+    });
+});
